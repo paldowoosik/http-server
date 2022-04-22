c34988281564b22a813b9cc993221cd9609dacdc
dgb980228 <dgb980228@naver.com>
2022-04-22 13:07:18 +0900

    task1 processed

--- src/main/java/com/nhnacademy/httpserver/Server.java
+++ src/main/java/com/nhnacademy/httpserver/Server.java
@@ -1,43 +1,81 @@
 package com.nhnacademy.httpserver;
+
+import static java.lang.System.lineSeparator;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
-    public static void main(String[] args ) {
-        try {
-            String data;
-            // 일단 포트번호 10000번으로 염
-            ServerSocket serverSocket = new ServerSocket(80);
-
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(80)) {
+            // 일단 포트번호 80번으로 염
+            System.out.println("서버 엶");
             Socket socket = serverSocket.accept();
-
+            System.out.println("연결 완료");
             BufferedReader bufferedReader =
-                new BufferedReader( new InputStreamReader(socket.getInputStream()));
+                new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            while((data = bufferedReader.readLine()) != null){
-                System.out.println(data);
-                // 여기가 read가 안끊기고 있음
+//            String data;
+//            while ((data = bufferedReader.readLine()) != null) {
+//                //client에 데이터 전송
+//                BufferedWriter bufferedWriter =
+//                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+//                bufferedWriter.write(data);
+//                bufferedWriter.newLine();
+////                bufferedWriter.flush();
+//
+//                System.out.println(data);
+//                // 여기가 read가 안끊기고 있음
+//            }
+
+            byte[] bytes = new byte[2048];
+            int numberOfBytes = socket.getInputStream().read(bytes);
+            String request = new String(bytes, 0, numberOfBytes, UTF_8);
+
+            StringBuilder response = new StringBuilder();
+
+            Map<String,String> payload = new HashMap<>();
+            String origin = socket.getInetAddress().getHostAddress();
+            payload.put("origin", origin);
+            String json = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(payload);
+
+
+            response.append("HTTP/1.1 200 OK").append(lineSeparator())
+                    .append("Date: " + new Date()).append(lineSeparator())
+                    .append("Content-Type: application/json").append(lineSeparator())
+                    .append("Content-Length: ").append(json.getBytes(UTF_8).length).append(lineSeparator())
+//                    .append("Connection: close").append(lineSeparator())
+                    .append("Server: gunicorn/19.9.0").append(lineSeparator())
+                    .append("Access-Control-Allow-Origin: *").append(lineSeparator())
+                    .append("Access-Control-Allow-Credentials: true").append(lineSeparator()).append(lineSeparator());
+
+            System.out.println(response);
+            response.append(json);
+
+
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+                writer.write(String.valueOf(response));
+                writer.flush();
             }
 
-//            //client에 데이터 전송
-//            BufferedWriter bufferedWriter =
-//                new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
-//            bufferedWriter.write("hello world");
-//            bufferedWriter.newLine();
-//            bufferedWriter.flush();
 
-            socket.close();
-            serverSocket.close();
-            bufferedReader.close();
+
+//            socket.close();
+//            bufferedReader.close();
 //            bufferedWriter.close();
-        }
 
-        catch( Exception e ){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
